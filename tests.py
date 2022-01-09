5cfc2bca3fae19fab45bfd698e808f0cdfc76d01
VictorHSMoura <victorhugosmoura@gmail.com>
2022-01-08 23:38:38 -0300

    Add final tests

--- tests.py
+++ tests.py
@@ -2,6 +2,25 @@ from unittest import TestCase
 
 from tree import *
 
+class TestNode(TestCase):
+    def setUp(self):
+        self.node = Node(5)
+
+    def test_create_node(self):
+        self.assertIsNotNone(self.node)
+        self.assertEqual(self.node.getValue(), 5)    
+
+    def test_insert_left(self):
+        self.node.insertLeft(Node(8))
+
+        self.assertEqual(self.node.getLeft().getValue(), 8)
+
+    def test_insert_right(self):
+        self.node.insertRight(Node(45))
+
+        self.assertEqual(self.node.getRight().getValue(), 45)
+
+
 class TestTree(TestCase):
     def setUp(self):
         self.tree = Tree()
@@ -12,7 +31,6 @@ class TestTree(TestCase):
     def test_add_root(self):
         self.tree.add(3)
 
-        self.assertIsNotNone(self.tree.getRoot())
         self.assertEqual(self.tree.getRoot().getValue(), 3)
 
     def test_add_left(self):
@@ -21,7 +39,6 @@ class TestTree(TestCase):
 
         root = self.tree.getRoot()
 
-        self.assertIsNotNone(root.getLeft())
         self.assertEqual(root.getLeft().getValue(), 1)
 
     def test_add_right(self):
@@ -29,5 +46,28 @@ class TestTree(TestCase):
         self.tree.add(5)
 
         root = self.tree.getRoot()
-        self.assertIsNotNone(root.getRight())
         self.assertEqual(root.getRight().getValue(), 5)
+
+    def test_find_node(self):
+        self.tree.add(3)
+        self.tree.add(5)
+        self.tree.add(7)
+
+        self.assertIsNotNone(self.tree.find(7))
+
+    def test_not_find_node(self):
+        self.tree.add(3)
+        self.tree.add(5)
+        self.tree.add(7)
+
+        self.assertIsNone(self.tree.find(9))
+
+    def test_delete_tree(self):
+        self.tree.add(3)
+        self.tree.add(5)
+        self.tree.add(7)
+
+        self.assertIsNotNone(self.tree.getRoot())
+
+        self.tree.deleteTree()
+        self.assertIsNone(self.tree.getRoot())
